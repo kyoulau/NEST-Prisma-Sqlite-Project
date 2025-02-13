14ae94834aa92854f24c1014b436bc1c19734f03
kyoulau <laucosan05@gmail.com>
2025-02-13 20:28:33 -0300

    add test for Create and findById

--- src/user/tests/user-service.spec.ts
+++ src/user/tests/user-service.spec.ts
@@ -1,5 +1,5 @@
 import { CreateUserDto } from './../userDTO/create-user-dto';
-import { Controller } from '@nestjs/common';
+import { Controller, HttpException, HttpStatus } from '@nestjs/common';
 //classe de teste para userService
 
 import { PrismaService } from "src/prisma/prisma.service";
@@ -8,6 +8,8 @@ import { UserService } from "../user.service"
 import { HashingServiceProtocol } from "src/auth/hash/bcrypt.service";
 import { Test } from "@nestjs/testing";
 import { create } from 'domain';
+import { find } from 'rxjs';
+import { Task } from 'src/data/database/task.entity';
 
 //Seguir Ordenação AAA 1- Configurar teste, algo que deseja fazer a ação e conferir se ação foi feita como foi esperada
 describe ('UserService', ()=> {
@@ -15,6 +17,7 @@ describe ('UserService', ()=> {
   let userController : UserController;
   let prismaService : PrismaService;
   let hashingService : HashingServiceProtocol;
+  let userID: 0
 
   beforeEach(async ()=>{
     const moduleRef = Test.createTestingModule({
@@ -25,10 +28,13 @@ describe ('UserService', ()=> {
           useValue:{
             user:{
               create: jest.fn().mockResolvedValue({
-                id: 1,
-                name: 'Laura',
-                email: 'laura@gmail.com'
-              })
+
+                username: 'Lauta teste unitario',
+                userEmail: 'testeunitario@gmail.com',
+                userRoleAtributed: 'USER'
+              }),
+              findFirst:jest.fn()
+              
             }
           }
         },
@@ -51,35 +57,83 @@ describe ('UserService', ()=> {
     expect(userService).toBeDefined();
   })
 
-  it('Should add an user', async ()=>{
+  describe('Create User', ()=>{
 
-    //melhorar esse método
-    const createUserDto: CreateUserDto = {
-      username: "Laura",
-      userEmail: "laura@gmail.com",
-      userPassword: "meuNamoradoLindo261076**",
-      userRoleAtributed:"USER"
-    };
+    it('Should add an user', async ()=>{
 
-    const mockUser = {
-      id: 1,
-      ...createUserDto,
-    };
-
-    jest.spyOn(hashingService, 'hashing').mockResolvedValue("EU_SOU_UM_HASH")
-
-    const result = await userService.createUser(createUserDto)
-
-    expect(hashingService.hashing).toHaveBeenCalled()
-
-    expect(prismaService.user.create).toHaveBeenCalledWith({
-      data:{
-        
+      const createUserDTO: CreateUserDto = {
+        username: 'Lauta teste unitario',
+        userEmail: 'testeunitario@gmail.com',
+        userPassword: 'HASHING_SENHA',
+        userRoleAtributed: 'USER'
       }
+  
+      //hash é gerado
+      const HASHING_SENHA = jest.spyOn(hashingService,'hashing').mockResolvedValue("HASH_SENHA")
+           
+       //cria novo usuário no prisma com dados do dto
+      let result = await  userService.createUser(createUserDTO)
+      console.log(result)
+  
+      expect(HASHING_SENHA).toHaveBeenCalled()
+  
+        //testar o retorno da função
+        expect(result).toEqual({
+          id: expect.any(Number),
+          username: createUserDTO.username,
+          userEmail: createUserDTO.userEmail
+        })
     })
-
-
   })
 
+  describe('Get User by id', ()=>{
+    it('Should return one user by ID', async ()=>{
+
+      //MOck usuario para ser encontrado.
+      const mockUserForFind = {
+        id: 68,
+        username: 'jungkookThePlayBoy',
+        userEmail: 'jungkook@gmail.com',
+        active: true,
+        userPassword: 'senhaUsuarioMaluco02**',
+        createdAt: new Date(),
+        userRoleAtributed: 'USER',
+        Task: [],
+        avatar: ''
+      }
+  
+      //mock para findFirst
+      jest.spyOn(prismaService.user, 'findFirst').mockResolvedValue(mockUserForFind)
+
+      const result = await userService.getUserById(68)
+
+      expect(prismaService.user.findFirst).toHaveBeenCalledWith({
+        where:{
+          id:68,
+        },
+        select:{
+          id:true,
+          userEmail:true,
+          username:true,
+          active:true,
+          createdAt:true,
+          userRoleAtributed:true,
+          avatar:true,
+          userPassword:true
+        }
+      })
+
+      expect(result).toEqual(mockUserForFind)
+  
+    })
+
+    it('Should throw an error if user is not found', async ()=>{
+      jest.spyOn(prismaService.user, 'findFirst').mockResolvedValue(null)
+
+      await expect(userService.getUserById(68)).rejects.toThrow(
+        new HttpException("Usuário não encontrado!",HttpStatus.NOT_FOUND)
+      )
+    })
+  })
   
 })
